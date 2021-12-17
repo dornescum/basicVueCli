f9e5d1557421b66df4cd2e989621f2b010791db7
Mihai Dornescu <dornescu@gmail.com>
2021-12-17 12:06:42 +0200

    vuex initial setup

---
+++ src/views/Counter.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="counter">
+    {{counter}}
+  </div>
+  <div class="buttons">
+    <button @click="decreaseCounter">-</button>
+    <button @click="increaseCounter">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data(){
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter(){
+      this.counter++
+    },
+    decreaseCounter(){
+      this.counter--
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  .buttons{
+    margin: 10px;
+    button {
+      margin: 10px;
+      padding: 10px;
+    }
+  }
+  .counter{
+    font-size: 80px;
+  }
+</style>
