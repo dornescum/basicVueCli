8249c87558ba3459094845b465f10e6c05b16747
Mihai Dornescu <dornescu@gmail.com>
2021-12-17 10:54:38 +0200

    custom events

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,13 +1,49 @@
 <template>
-$END$
+<!--?  declar aici functia pe care o folosesc in alt component-->
+  <div class="backdrop" @click="closeModal">
+<!--    a 2 a clasa este dinamica(pot adauga mai multe)-->
+<!--!    props are optional-->
+    <div class="modal" :class="{sale: theme ==='sale'}">
+     <p>{{title}}</p>
+     <p>{{subTitle}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Modal"
-}
+  name: "Modal",
+  // declar aici props si il folosesc unde am nevoie
+  props:['title', 'subTitle', 'theme'],
+  methods: {
+    closeModal(){
+      //! emit un event de aici . from parrent to child
+      this.$emit('close') //! si ii dau ce nume vreau eu pe care il folosesc in Home (in cazul asta)
+    }
+  }
+};
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .modal{
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 15px 0;
+      p{
+        font-style: italic;
+      }
+  }
+  .sale {
+    background: lightpink;
+    color: #fff;
+  }
+  .backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>testing vue</p>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div v-if="showModal">
+<!-- ?     nume eventului declarat in Modal => close-->
+      <Modal :title="title" :sub-title="subTitle" theme="sale" @close="toggleModal"/>
+    </div>
+    <button @click="toggleModal">show modal</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Modal from "@/components/Modal";
 
 export default {
+
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
+    Modal
+  },
+  data(){
+    return {
+      title: 'modal in vue',
+      subTitle: 'subtitle in vue',
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
