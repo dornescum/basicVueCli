26e062a1baf37c22d876304997e65a5e02595965
Mihai Dornescu <dornescu@gmail.com>
2021-12-17 13:36:21 +0200

    actions fetch

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,8 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    counter: 0
+    counter: 0,
+    name: ''
   },
   mutations: {
     increaseCounter(state){
@@ -10,9 +11,20 @@ export default createStore({
     },
     decreaseCounter(state){
       state.counter--
+    },
+    showName(state, name){
+      console.log(name);
+      state.name = name
     }
   },
   actions: {
+    // for asynchronous code
+    showName({commit}){
+      console.log('showName (action)');
+      fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
+          // .then(json => console.log(json[0].name))
+          .then(json => commit('showName',  json[0].name))
+    }
   },
   modules: {
   }
--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -7,19 +7,26 @@
 <!--!    dc actiunea vine din mutations => commit-->
     <button @click="$store.commit('decreaseCounter')">-</button>
     <button @click="$store.commit('increaseCounter')">+</button>
+    <button @click="$store.dispatch('showName')">async test</button>
 <!--    <button @click="decreaseCounter">-</button>-->
 <!--    <button @click="increaseCounter">+</button>-->
   </div>
+
+
+    <p >{{$store.state.name}}</p>
+
+
 </template>
 
 <script>
 export default {
   name: "Counter",
-  // data(){
-  //   return {
-  //     counter: 0
-  //   }
-  // },
+  data(){
+    return {
+      // counter: 0
+      // isVisible: false
+    }
+  },
   methods: {
     // increaseCounter(){
     //   this.counter++
