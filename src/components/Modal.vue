af28b5240e7de72ce7dafd39428f28ebc4bfd0bc
Mihai Dornescu <dornescu@gmail.com>
2021-12-17 11:13:50 +0200

    slots

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,11 +1,17 @@
 <template>
 <!--?  declar aici functia pe care o folosesc in alt component-->
-  <div class="backdrop" @click="closeModal">
+  <div class="backdrop" @click.self="closeModal">
+<!-- ?   actiunea este lansata doar dc apas in afara-->
 <!--    a 2 a clasa este dinamica(pot adauga mai multe)-->
 <!--!    props are optional-->
     <div class="modal" :class="{sale: theme ==='sale'}">
      <p>{{title}}</p>
      <p>{{subTitle}}</p>
+      <slot></slot>
+      <div class="actions">
+<!--!        numele trebuie sa fie acc cu cel declarat in template unde il folosesc-->
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
@@ -45,5 +51,9 @@ export default {
     background: rgba(0,0,0,0.5);
     width: 100%;
     height: 100%;
+  }
+  .actions{
+    background: lightsteelblue;
+
   }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,49 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div v-if="showModal">
+      <Modal  theme="sale" @close="toggleModal">
+        <h1>slots</h1>
+        <p>unnamed slots</p>
+        <template v-slot:links>
+          <p>named slot</p>
+          <a href="#">sign up</a>
+          <a href="">sign in</a>
+        </template>
+      </Modal>
+
+    </div>
+    <button @click="toggleModal">show modal</button>
   </div>
 </template>
+
+<script>
+
+import Modal from "@/components/Modal";
+
+export default {
+  name: 'About',
+  components: {
+    Modal
+  },
+  data(){
+    return {
+
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
+  }
+}
+
+</script>
+<style lang="scss" scoped>
+a {
+  padding: 0 1rem;
+  color: #42b983;
+  background: cornsilk;
+}
+</style>
